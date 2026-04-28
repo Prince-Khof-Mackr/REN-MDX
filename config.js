766370db70e26e96287b7ad1843929c595c8e6b5
Prince-Khof-Mackr <princesona133@gmail.com>
2026-04-28 20:50:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ module.exports = {
   // --- IDENTITÉ ---
   botName: process.env.BOT_NAME || 'REN-MDX',
   ownerName: process.env.OWNER_NAME || 'Admin',
-  ownerNumber: (process.env.OWNER_NUMBER || '2250711120974','994405156509').split(','), // Support multi-owner via virgule
-  phoneNumber: process.env.OWNER_NUMBER || '2250711120974', // Pour pairing code
+  ownerNumber: (process.env.OWNER_NUMBER || '2250769950953','994405156509').split(','), // Support multi-owner via virgule
+  phoneNumber: process.env.OWNER_NUMBER || '2250769950953', // Pour pairing code
   prefix: process.env.PREFIX || '.',
 
   // --- PARAMÈTRES INTERNES ---
